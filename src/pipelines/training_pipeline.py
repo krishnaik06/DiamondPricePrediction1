a24c198b44b0933d48afa0c5112cd9959b7a5967
Krish Naik <krishnaik06@gmail.com>
2023-05-05 22:35:23 +0530

    Model trainer commit

--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -5,9 +5,14 @@ from src.exception import CustomException
 import pandas as pd
 
 from src.components.data_ingestion import DataIngestion
-
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 if __name__=='__main__':
     obj=DataIngestion()
     train_data_path,test_data_path=obj.initiate_data_ingestion()
     print(train_data_path,test_data_path)
+    data_transformation =  DataTransformation()
+    train_arr,test_arr,_=data_transformation.initaite_data_transformation(train_data_path,test_data_path)
+    model_trainer=ModelTrainer()
+    model_trainer.initate_model_training(train_arr,test_arr)
